a78d272b37bcd6fe8e7d63381ccf340af447d502
Bosco Ho <apposite@gmail.com>
2013-11-28 15:06:12 +1100

    new version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ Docs at http://github.com/boscoh/tohaml.
 
 setup(
     name='tohaml',
-    version='1.0a1',
+    version='1.0a2',
     author='Bosco Ho',
     author_email='boscoh@gmail.com',
     url='http://github.com/boscoh/tohaml',
