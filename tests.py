effb0fa392c7e4d1d90fde3289651aeeacb57392
David Su <dtscraft@gmail.com>
2014-07-04 09:15:29 -0700

    test self closing tag, inline text, attributes
    inline text is currently rendered with a newline after the tag
    surrounding the text. should be fixed later

--- tests.py
+++ tests.py
@@ -6,8 +6,16 @@ class HtmlToHamlPyTest(unittest.TestCase):
         self.assertEqual('.foo', tohaml.render('<div class="foo"> </div>'))
     def test_multiple_class_names(self):
         self.assertEqual('.foo.bar.baz', tohaml.render('<div class=" foo bar baz "></div>'))
+    def test_should_have_pretty_attributes(self):
+        self.assertEqual('%input{type:"text",name:"login"}', tohaml.render('<input type="text" name="login" />'))
+        self.assertEqual('%meta{content:"text/html",http-equiv:"Content-Type"}', tohaml.render('<meta http-equiv="Content-Type" content="text/html" />'))
     def test_id_with_dot_and_hash(self):
         self.assertEqual('%div{id:"foo.bar"}', tohaml.render("<div id='foo.bar'></div>"))
         self.assertEqual('%div{id:"foo#bar"}', tohaml.render("<div id='foo#bar'></div>"))
+    def test_self_closing_tag(self):
+        self.assertEqual("%img", tohaml.render("<img />"))
+    def test_inline_text(self):
+        #FIXME
+        self.assertEqual("%p\n  foo", tohaml.render("<p>foo</p>"))
 if __name__ == '__main__':
     unittest.main()
