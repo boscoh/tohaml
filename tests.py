ee562b25fe50efa759da1e7c73b3631922db25d1
David Su <dtscraft@gmail.com>
2014-07-04 09:14:43 -0700

    test no tag for div if class or id present

--- tests.py
+++ tests.py
@@ -1,6 +1,9 @@
 from  test_helper import *
 
 class HtmlToHamlPyTest(unittest.TestCase):
+    def test_no_tag_name_for_div_if_class_or_id_is_present(self):
+        self.assertEqual('#foo', tohaml.render('<div id="foo"> </div>'))
+        self.assertEqual('.foo', tohaml.render('<div class="foo"> </div>'))
     def test_multiple_class_names(self):
         self.assertEqual('.foo.bar.baz', tohaml.render('<div class=" foo bar baz "></div>'))
     def test_id_with_dot_and_hash(self):
